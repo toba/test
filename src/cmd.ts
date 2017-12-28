45fa4185afef6acedd5f1a7941171d51f9e3305f
Jason Abbott <dev@trailimage.com>
2017-12-28 13:59:18 -0700

    initial bin stuff

---
+++ src/cmd.ts
@@ -0,0 +1,30 @@
+/* tslint:disable:no-unused */
+import * as Mocha from "mocha";
+import * as fs from "fs";
+import * as path from "path";
+import "ignore-styles";
+import "ts-node/register";
+import "tsconfig-paths/register";
+
+// node_modules/.bin/mocha
+// --require ts-node/register
+// --require ignore-styles
+// --require tsconfig-paths/register
+// ./**/*.test.ts*"
+
+const mocha = new Mocha();
+const testDir = "src";
+
+// https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically
+fs
+   .readdirSync(testDir)
+   .filter(file => file.substr(-3) === ".ts")
+   .forEach(file => {
+      mocha.addFile(path.join(testDir, file));
+   });
+
+mocha.run(failures => {
+   process.on("exit", () => {
+      process.exit(failures);
+   });
+});
---
+++ src/component.test.ts
@@ -0,0 +1,9 @@
+/* tslint:disable:no-unused-expression */
+import "mocha";
+import { expect } from "chai";
+
+describe("Component Tester", () => {
+   it("does stuff", () => {
+      expect(false).is.false;
+   });
+});
