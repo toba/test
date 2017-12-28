ceb602bfd52458ca957aa13b9c72d5afb5d95e84
Jason Abbott <dev@trailimage.com>
2017-12-28 15:39:17 -0700

    mocha madness

--- src/cmd.ts
+++ src/cmd.ts
@@ -2,8 +2,8 @@
 import * as Mocha from "mocha";
 import * as fs from "fs";
 import * as path from "path";
-import "ts-node/register";
-import "tsconfig-paths/register";
+import * as tsnode from "ts-node";
+//import "tsconfig-paths/register";
 import "ignore-styles";
 
 // node_modules/.bin/mocha
@@ -14,13 +14,16 @@ import "ignore-styles";
 
 const mocha = new Mocha();
 const testDir = "src";
+const reg = tsnode.register({
+   cacheDirectory: "./dist"
+});
 
 // https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically
 fs
-   .readdirSync(testDir)
-   .filter(file => file.substr(-3) === ".ts")
+   .readdirSync(reg.cachedir)
+   //.filter(file => file.substr(-7) === "test.ts")
    .forEach(file => {
-      mocha.addFile(path.join(testDir, file));
+      mocha.addFile(path.join(reg.cachedir, file));
    });
 
 mocha.run(failures => {
