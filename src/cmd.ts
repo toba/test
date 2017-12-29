b32c561b072319adc22a15cbda9911aa9498de6a
Jason Abbott <dev@trailimage.com>
2017-12-29 13:05:56 -0700

    functional

--- src/cmd.ts
+++ src/cmd.ts
@@ -2,28 +2,19 @@
 import * as Mocha from "mocha";
 import * as fs from "fs";
 import * as path from "path";
-import * as tsnode from "ts-node";
-//import "tsconfig-paths/register";
+import "ts-node/register";
+import "tsconfig-paths/register";
 import "ignore-styles";
 
-// node_modules/.bin/mocha
-// --require ts-node/register
-// --require ignore-styles
-// --require tsconfig-paths/register
-// ./**/*.test.ts*"
-
 const mocha = new Mocha();
-const testDir = "src";
-const reg = tsnode.register({
-   cacheDirectory: "./dist"
-});
+const testDir = "./src/";
 
 // https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically
 fs
-   .readdirSync(reg.cachedir)
-   //.filter(file => file.substr(-7) === "test.ts")
+   .readdirSync(testDir)
+   .filter(file => file.substr(-7) === "test.ts")
    .forEach(file => {
-      mocha.addFile(path.join(reg.cachedir, file));
+      mocha.addFile(path.join(testDir, file));
    });
 
 mocha.run(failures => {
