6a4ff157abbb26fffcac3eb397fc7d07db3f5257
Jason Abbott <dev@trailimage.com>
2018-03-04 12:37:07 -0700

    add local-storage mock setup

--- jest/index.js
+++ jest/index.js
@@ -4,27 +4,13 @@
  */
 module.exports = {
    transform: {
-      "^.+\\.tsx?$": "ts-jest"
+      '^.+\\.tsx?$': 'ts-jest'
    },
+   setupFiles: ['jest-localstorage-mock'],
    collectCoverage: true,
-   collectCoverageFrom: [
-      "lib/*.ts"
-   ],
-   coverageReporters: [
-      "lcov"
-   ],
-   transformIgnorePatterns: [
-      "<rootDir>/node_modules/(?!@toba)"
-   ],
-   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-   moduleFileExtensions: [
-      "ts",
-      "tsx",
-      "js",
-      "jsx",
-      "json",
-      "node"
-   ]
+   collectCoverageFrom: ['lib/*.ts'],
+   coverageReporters: ['lcov'],
+   transformIgnorePatterns: ['<rootDir>/node_modules/(?!@toba)'],
+   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
-
-
