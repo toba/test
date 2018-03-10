fc99a972aef82af54ebaab3c984acbd784f308b0
Jason Abbott <dev@trailimage.com>
2018-03-10 16:51:04 -0700

    use standard expect extension

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,10 +1,42 @@
-export function expectWithin(target: number, min: number, max: number) {
-   expect(target).toBeLessThanOrEqual(max);
-   expect(target).toBeGreaterThanOrEqual(min);
+interface ExpectResponse {
+   message: () => string;
+   pass: boolean;
 }
 
-export function expectAllProperties(target: Object, ...keys: string[]) {
-   keys.forEach(k => {
-      expect(target).toHaveProperty(k);
-   });
-}
+/**
+ * https://facebook.github.io/jest/docs/en/expect.html#expectextendmatchers
+ */
+expect.extend({
+   toBeWithin(received: number, min: number, max: number): ExpectResponse {
+      const pass = received >= min && received <= max;
+      return pass
+         ? {
+              message: () =>
+                 `expected ${received} not to be within ${min} and ${max}`,
+              pass
+           }
+         : {
+              message: () =>
+                 `expected ${received} to be within ${min} and ${max}`,
+              pass
+           };
+   },
+   toHaveAllProperties(received: Object, ...keys: string[]): ExpectResponse {
+      const missing = keys.filter(k => !received.hasOwnProperty(k));
+      const pass = missing.length == 0;
+
+      return pass
+         ? {
+              message: () =>
+                 `expected object to not have all properties ${keys.join(
+                    ', '
+                 )}`,
+              pass
+           }
+         : {
+              message: () =>
+                 `expected object to have properties ${missing.join(', ')}`,
+              pass
+           };
+   }
+});
