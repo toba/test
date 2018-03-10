83ab8792e3ee6ec92907cd3febcbbeacf0297bcf
Jason Abbott <dev@trailimage.com>
2018-03-10 12:43:54 -0700

    remove node-fetch to avoid circ dep

--- index.ts
+++ index.ts
@@ -1,15 +1,13 @@
 import * as fs from 'fs';
-import { Request, RequestInit, Response } from 'node-fetch';
 
 /** http://www.lipsum.com/ */
 export const lipsum =
    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-class MockResponse extends Response {
+class MockResponse {
    plainText: string;
 
    constructor(body: string) {
-      super();
       this.plainText = body;
    }
    text(): Promise<string> {
