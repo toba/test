29597eabc6b2a27b9fcfb990a615b5b0ec0980a9
Jason Abbott <dev@trailimage.com>
2018-03-24 21:48:38 -0600

    update mock http stuff

--- lib/mock-request.ts
+++ lib/mock-request.ts
@@ -1,19 +1,57 @@
-export class MockRequest {
+import { ServerResponse, IncomingMessage } from 'http';
+import { Socket } from 'net';
+
+export class MockSocket extends Socket {
+   /**
+    * https://nodejs.org/api/net.html#net_socket_remoteaddress
+    */
+   mockRemoteAddress: string;
+
+   get remoteAddress() {
+      return this.mockRemoteAddress;
+   }
+
+   reset() {
+      this.mockRemoteAddress = '';
+   }
+}
+
+/**
+ * Mock functionality of basic Node request and include field accessors to
+ * satisfy Express.
+ */
+export class MockRequest extends IncomingMessage {
    referer: string;
-   accepts: string;
    /** Querystring parameters */
    params: { [key: string]: string };
    headers: { [key: string]: string };
-   clientIP: string;
-
-   connection: {
-      remoteAddress: string;
-   };
+   protocol: string;
+   secure: boolean;
+   ip: string;
+   ips: string[];
+   connection: MockSocket;
+   subdomains: string[];
+   path: string;
+   hostname: string;
+   host: string;
+   fresh: boolean;
+   stale: boolean;
+   xhr: boolean;
+   cookies: any;
+   method: string;
+   query: any;
+   route: any;
+   signedCookies: any;
+   originalUrl: string;
+   url: string;
+   baseUrl: string;
+   app: any;
 
    // added by Express body parser
    body: { selected: string[] };
 
    constructor() {
+      super(new MockSocket());
       this.reset();
    }
 
@@ -28,15 +66,63 @@ export class MockRequest {
       }
    }
 
-   header(name: string): string {
+   header(name: 'set-cookie'): string[] | undefined;
+   header(name: string): string | undefined;
+   header(name: string | 'set-cookie'): string | string[] {
       return this.headers[name];
    }
+
+   accepts(): string[];
+   accepts(type: string): string | false;
+   accepts(type: string[]): string | false;
+   accepts(_type?: string | string[]): string | string[] | false {
+      return false;
+   }
+
+   acceptsCharsets(): string[];
+   acceptsCharsets(charset: string): string | false;
+   acceptsCharsets(charset: string[]): string | false;
+   acceptsCharsets(_charset?: string | string[]): string | string[] | false {
+      return false;
+   }
+
+   acceptsEncodings(): string[];
+   acceptsEncodings(encoding: string): string | false;
+   acceptsEncodings(encoding: string[]): string | false;
+   acceptsEncodings(_encoding?: string | string[]): string | string[] | false {
+      return false;
+   }
+
+   acceptsLanguages(): string[];
+   acceptsLanguages(lang: string): string | false;
+   acceptsLanguages(lang: string[]): string | false;
+   acceptsLanguages(_lang?: string | string[]): string | string[] | false {
+      return false;
+   }
+
+   range(_size: number): any {
+      return null;
+   }
+
+   param(_name: string, _defaultValue?: any): string {
+      return null;
+   }
+
+   is(_type: string): string | false {
+      return false;
+   }
+
+   clearCookie(_name: string, _options?: any): any {
+      return null;
+   }
+
+   accepted: any[];
 
    reset(): MockRequest {
       this.referer = null;
       this.params = {};
       this.headers = {};
-      this.connection = { remoteAddress: '' };
+      this.connection.reset();
       this.body = { selected: [] };
       return this;
    }
--- lib/mock-response.ts
+++ lib/mock-response.ts
@@ -1,21 +1,30 @@
+import { HttpStatus, MimeType, Encoding, Header, is, merge } from '@toba/tools';
 import * as util from 'util';
-import { HttpStatus, MimeType, Encoding, is } from '@toba/tools';
+import { ServerResponse, IncomingMessage } from 'http';
+import { Server } from 'net';
 
-export class MockResponse {
+/**
+ * Mock Node HTTP response with additional methods to capture end and redirect.
+ */
+export class MockResponse extends ServerResponse {
    httpStatus: HttpStatus;
    /**
     * Method to call when response is complete. Can be assigned as test
     * middleware next() method so that response.end() and middelware next()
-    * are both captured
+    * are both captured. It defaults to an empty Jest mock.
     */
-   onEnd: () => void;
+   onEnd = jest.fn();
 
    /** Whether response should be ended after render is called */
    endOnRender: boolean;
-
+   app: any;
    ended: boolean;
    headers: { [key: string]: string };
    content: string;
+   send: any;
+   jsonp: any;
+   locals: any;
+   charset: string;
    rendered: {
       template: string;
       options: { [key: string]: any };
@@ -30,18 +39,17 @@ export class MockResponse {
       url: string;
    };
 
-   constructor() {
+   constructor(req: IncomingMessage) {
+      super(req);
       this.reset();
    }
 
-   status(value: HttpStatus): MockResponse {
-      this.httpStatus = value;
+   status(code: number): any {
+      this.httpStatus = code;
       return this;
    }
 
-   notFound(): MockResponse {
-      return this.status(HttpStatus.NotFound);
-   }
+   //status(code: number): Response;
 
    setHeader(key: string, value: string): MockResponse {
       this.headers[key] = value;
@@ -51,31 +59,54 @@ export class MockResponse {
    /**
     * Set header value(s)
     */
+   set(field: any): any;
+   set(field: string, value?: string): any;
    set(keyOrHash: string | { [key: string]: string }, value: string = null) {
       if (typeof keyOrHash == is.Type.String) {
          this.headers[keyOrHash as string] = value;
       } else {
-         Object.assign(this.headers, keyOrHash);
+         merge(this.headers, keyOrHash);
       }
    }
 
-   redirect(status: HttpStatus, url: string) {
+   sendStatus(_code: number): any {
+      return null;
+   }
+
+   links(_links: any): any {
+      return null;
+   }
+
+   redirect(url: string): void;
+   redirect(url: string, status: number): void;
+   redirect(status: HttpStatus, url: string): void;
+   redirect(first: string | number | HttpStatus, second?: string | number) {
+      let status: HttpStatus;
+      let url: string;
+      if (is.number(first)) {
+         status = first;
+         url = second as string;
+      } else {
+         status = second as number;
+         url = first;
+      }
       this.redirected.status = status;
       this.redirected.url = url;
       this.end();
    }
 
    /**
-    * Method added by Express
+    * Express method.
     */
-   json(o: any) {
+   json(o: any): any {
       this.httpStatus = HttpStatus.OK;
       this.rendered.json = o;
-      return this.setHeader('Content-Type', MimeType.JSON).end();
+      return this.setHeader(Header.Content.Type, MimeType.JSON).end();
    }
 
    /**
-    * Serialize render options rather than actually rendering a view
+    * Express method. Serialize render options rather than actually rendering a
+    * view.
     */
    render(
       template: string,
@@ -97,16 +128,20 @@ export class MockResponse {
    /**
     * https://nodejs.org/api/stream.html#stream_class_stream_writable
     */
-   write(
-      chunk: string | Buffer,
-      encoding = Encoding.UTF8,
-      callback?: () => void
-   ) {
+   write(chunk: any, encodingOrCb?: string | Function, cb?: Function) {
+      let encoding: string = Encoding.UTF8;
+
+      if (cb !== undefined) {
+         encoding = encodingOrCb as string;
+      } else {
+         cb = encodingOrCb as Function;
+      }
+
       const text = Buffer.isBuffer(chunk) ? chunk.toString(encoding) : chunk;
       this.content = this.content === null ? text : this.content + text;
 
-      if (is.callable(callback)) {
-         callback();
+      if (is.callable(cb)) {
+         cb();
       }
       return true;
    }
@@ -123,7 +158,6 @@ export class MockResponse {
 
    reset(): MockResponse {
       this.httpStatus = HttpStatus.OK;
-      this.onEnd = null;
       this.ended = false;
       this.headers = {};
       this.content = null;
@@ -137,6 +171,79 @@ export class MockResponse {
          status: null,
          url: null
       };
+
       return this;
+   }
+
+   sendFile(path: string): void;
+   sendFile(path: string, options: any): void;
+   sendFile(path: string, fn: Function): void;
+   sendFile(_path: string, _options?: any, _fn?: Function): void {
+      return;
+   }
+
+   sendfile(path: string): void;
+   sendfile(path: string, options: any): void;
+   sendfile(path: string, fn: Function): void;
+   sendfile(_path: string, _options?: any, _fn?: Function): void {
+      return;
+   }
+
+   download(path: string): void;
+   download(path: string, filename: string): void;
+   download(path: string, fn: Function): void;
+   download(
+      _path: string,
+      _filename?: string | Function,
+      _fn?: Function
+   ): void {
+      return;
+   }
+
+   contentType(_type: string): any {
+      return null;
+   }
+
+   type(_type: string): any {
+      return null;
+   }
+
+   format(_obj: any): any {
+      return null;
+   }
+
+   attachment(_filename?: string): any {
+      return null;
+   }
+
+   header(field: any): any;
+   header(_field: string, _value?: string): any {
+      return null;
+   }
+
+   get(_field: string): string {
+      return null;
+   }
+
+   cookie(name: string, val: any): any;
+   cookie(name: string, val: any, options: any): any;
+   cookie(_name: string, _val: any, _options?: any): any {
+      return null;
+   }
+
+   clearCookie(_name: string, _options?: any): any {
+      return null;
+   }
+
+   location(_url: string): any {
+      return null;
+   }
+
+   vary(_field: string): any {
+      return null;
+   }
+
+   append(_field: string, _value?: string[] | string): any {
+      return null;
    }
 }
