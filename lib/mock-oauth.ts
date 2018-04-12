e909a2debdf36ee59d7e72e8fb4f43b442359253
Jason Abbott <dev@trailimage.com>
2018-04-11 20:27:08 -0600

    rename to mockAuth to avoid ambiguity

--- lib/mock-oauth.ts
+++ lib/mock-oauth.ts
@@ -4,9 +4,9 @@ import fetch from 'node-fetch';
  * Mock the OAuth client imported by `@toba/oauth` by adding an `oauth.ts` to
  * a `__mocks__` folder in the project root with
  *
- * `export { OAuth } from '@toba/test'`
+ * `export { MockAuth as OAuth } from '@toba/test'`
  */
-export class OAuth {
+export class MockAuth {
    urls: { [key: string]: string };
    last: {
       accessToken: string;
