c660ed75352f91cc758d7268c768a081deef8b08
Jason Abbott <dev@trailimage.com>
2018-06-30 11:21:21 -0600

    add more helpers to memory stream

--- lib/memory-stream.test.ts
+++ lib/memory-stream.test.ts
@@ -4,9 +4,14 @@ import { loadStream } from './index';
 
 test('recieves stream', done => {
    const stream = new MemoryStream();
+   expect(stream.writeWasCalled).toBe(false);
+   expect(stream.receivedData).toBe(false);
+
    loadStream('helpers.ts')
       .on('end', () => {
          expect(stream.text.includes('ExpectResponse')).toBe(true);
+         expect(stream.writeWasCalled).toBe(true);
+         expect(stream.receivedData).toBe(true);
          done();
       })
       .pipe(stream);
--- lib/memory-stream.ts
+++ lib/memory-stream.ts
@@ -6,12 +6,26 @@ import { Encoding, is } from '@toba/tools';
  */
 export class MemoryStream extends Duplex {
    text: string = '';
+   /**
+    * Whether data (not `null` and not zero-length) was received.
+    */
+   receivedData: boolean = false;
+   /**
+    * Whether the `write()` method was called regardless of whether any data
+    * was actually received.
+    */
+   writeWasCalled: boolean = false;
 
    _write(data: Buffer, encoding: Encoding, cb?: () => void) {
       if (encoding == Encoding.Buffer) {
          this.text += data.toString(Encoding.UTF8);
       } else {
          this.text += data;
+      }
+      this.writeWasCalled = true;
+
+      if (data.length > 0) {
+         this.receivedData = true;
       }
 
       if (is.callable(cb)) {
