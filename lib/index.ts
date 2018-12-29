1d83dda7ef24cd79c427944c57f72d74f1ce29d1
Jason Abbott <dev@trailimage.com>
2018-12-28 17:51:01 -0700

    add fetch ref

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
+import 'jest-fetch-mock';
 import { log } from '@toba/logger';
 // mockFetch must be imported before OAuth since OAuth may utilize a node-fetch mock
 export { mockFetch } from './mock-fetch';
