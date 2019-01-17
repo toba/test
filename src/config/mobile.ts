65369c31c6c2e9305eccc966fadeef20d6185579
Jason Abbott <dev@trailimage.com>
2019-01-16 20:55:04 -0700

    add install script to patch rn jest

--- src/config/mobile.ts
+++ src/config/mobile.ts
@@ -1,11 +1,15 @@
 import * as baseConfig from './';
 
+/**
+ * Configuration for testing React Native components depends on `react-native`
+ * having been installed.
+ */
 const config: Partial<jest.DefaultOptions> = {
    ...baseConfig,
-   //preset: 'react-native',
+   preset: 'react-native',
    setupFiles: [
       ...baseConfig.setupFiles!,
-      '<rootDir>/node_modues/react-native/jest/setup.js'
+      '<rootDir>/node_modules/react-native/jest/setup.js'
    ]
 };
 
--- src/config/stub.ts
+++ src/config/stub.ts
@@ -1,3 +1,3 @@
 // https://jestjs.io/docs/en/webpack#handling-static-assets
 
-export default 'test-file-stub';
+export = 'test-file-stub';
