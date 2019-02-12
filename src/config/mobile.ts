95a2e85926567b1273221dcee08f41ecfa29fc80
Jason Abbott <jasonabbott@Jason-Abbot-MBP15tb.local>
2019-02-12 11:26:32 -0700

    add doc

--- src/config/mobile.ts
+++ src/config/mobile.ts
@@ -3,6 +3,8 @@ const baseConfig: Partial<jest.DefaultOptions> = require('./');
 /**
  * Configuration for testing React Native components depends on `react-native`
  * having been installed.
+ *
+ * @see https://github.com/expo/expo/issues/2595
  */
 const config: Partial<jest.DefaultOptions> = {
    ...baseConfig,
